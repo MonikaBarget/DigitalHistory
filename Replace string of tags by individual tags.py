249fa457a04db8111d3135d8478438d437eade50
Monika Barget <monika.barget@mu.ie>
2020-02-07 11:22:23 +0100

    Pyzotero: replace string of tags by unique tags
    This script was written to solve a common problem that occurs when importing bibliographic information from library catalogues to ZOTERO.
    In many cases, several tags come in as strings. In my case, the individual tags were separated by double dashes and spaces. The script identified individual tags in the old tags and added those words as new tags. You may run the script multiple times on the same library as ZOTERO automatically avoids duplicates. Also, you may replace the separator between tags by whatever occurs in your library. Also, the script does NOT delete your old tags. You may do so in the ZOTERO selector or use a different script. I will provide more scripts based on pyzotero in this repository soon.
    
    Occassionally, the following error occurs while running the script:
    
    PreConditionFailed: 
    Code: 412
    URL: https://api.zotero.org/groups/2442427/items/WJGVAMKP
    Method: PATCH
    Response: Item has been modified since specified version (expected 352, found 353)
    
    This is due to the syncing process of your ZOTERO library but does not affect the functionality of the script. Simply run the script again if you doubt that all new tags have been added correctly.

---
+++ Replace string of tags by individual tags.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+# Script for manipulating tags in ZOTERO
+# library is available on github 
+# documentation: https://pyzotero.readthedocs.io/en/latest/
+
+from pyzotero import zotero 
+import pandas as pd
+
+libnumber='XXXXXXX' # add your own user ID '2287217' for private library or ID in URL of group-library
+libtype='group' # “user” or “group”
+libapikey='YYYYYYYYYYYY' # add your own ZOTERO API key generated in your account settings (cf. "feeds-api")
+
+# access your library
+zot = zotero.Zotero(libnumber, libtype, libapikey) 
+# count number of top-level items in ZOTERO and print result
+print("There are", zot.num_items(), "items in your selected library.") 
+
+# identify current ZOTERO tags using double dash and spaces as separators
+oldtags=[] 
+oldtags=zot.tags(q=(" -- "))
+# print number of old tags
+print("There are", len(oldtags), "old tags that need updating.") 
+# print list of old tags
+print("These are the outdated tags in your library:", oldtags) 
+
+# create list of new tags including repetitions
+newtags=[]
+# create list of unique new tags
+newtags_flat=[] 
+
+# split old tags after double dash and spaces
+for t in oldtags:
+    newtag=t.split(" -- ") 
+    newtags.append(newtag)
+# print nested list of split tags
+print("This is the nested list of all split tags:", newtags) 
+
+# flatten nested list and avoid repetition of new tags
+for sublist in newtags:
+    for i in sublist:
+        if i in newtags_flat:
+            continue 
+        else: newtags_flat.append(i) 
+# print flattened list of split tags            
+print("This is the flattened list of new tags:", newtags_flat) 
+print(len(newtags_flat), "new tags need to be added.")
+
+replacetags=[] # this list is a fraction of "oldtags" but necessary as "tag" in Pyzotero only permits exact matches
+for newtag_flat in newtags_flat:
+    replacetags=zot.tags(q=newtag_flat) # instead of looking for " -- ", we now identify tags by individual words
+    for replacetag in replacetags:
+        affecteditems=zot.items(tag=replacetag) # identify items whose tags contain the particular words
+        for affecteditem in affecteditems:
+            zot.add_tags(affecteditem, newtag_flat) # new tags are added one by one
+
+print("Update completed")
+
+
+# In[ ]:
+
+
+
+
