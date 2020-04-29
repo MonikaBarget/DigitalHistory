7ae34f77203a7a28c176fc251f3afa40697fff28
Monika Barget <monika.barget@mu.ie>
2020-04-29 13:22:04 +0200

    documentation added

--- ReadXML_titles_withBeautifulSoup.py
+++ ReadXML_titles_withBeautifulSoup.py
@@ -1,42 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
-
-
 from bs4 import BeautifulSoup
 import os
 from os.path import dirname, join
-directory=("C:\\Users\\mbarg\\Documents\\corpus")
+directory=("C:\\Users\\mbarg\\Documents\\corpus") # location of XML files on local drive
 
-results=[]
+results=[] # create result list
 for infile in os.listdir(directory):
     filename=join(directory, infile)
-    indata=open(filename,"r", encoding="utf-8", errors="ignore") 
+    indata=open(filename,"r", encoding="utf-8", errors="ignore") # UTF-8 encoding errors are ignored
     contents = indata.read()
     soup = BeautifulSoup(contents,'xml')
-    titles = soup.find_all('title')
+    titles = soup.find_all('title') # get item titles
     for title in titles:
         print(title.get_text())
         results.append(title.get_text())
-print(results)
-        
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+print(results) # result list is shown on screen
