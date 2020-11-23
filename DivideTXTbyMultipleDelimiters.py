c4f30c3526bea836729401409e49aadc46c308b8
Monika Barget <monika.barget@mu.ie>
2020-11-23 18:57:19 +0100

    Update DivideTXTbyMultipleDelimiters.py

--- DivideTXTbyMultipleDelimiters.py
+++ DivideTXTbyMultipleDelimiters.py
@@ -1,8 +1,7 @@
+# SCRIPT TO READ SEMI-STRUCTURED BIBLIOGRAPHIC DATA FROM TXT AND WRITE THEM TO CSV
+
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[77]:
-
 
 import os
 import os.path
@@ -10,8 +9,7 @@ from os.path import dirname, join
 import re
 import csv
 
-delimiters=["Geburtsbrief für ", "Aussteller: ", "S. "]
-search_terms=["insul", "insel", "insuln", "inseln", "insula", "insulae"]
+delimiters=["Geburtsbrief für ", "Aussteller: ", "S. "] # Keywords that indicate a new type of information
 in_file="C:\\Users\\mobarget\\Google Drive\\ACADEMIA\\9_INSULAE\\SignaturTitel_GeburtsbriefeMainz_1-100.txt"
 out_directory="C:\\Users\\mobarget\\Google Drive\\ACADEMIA\\9_INSULAE"
 out_file=os.path.join(out_directory, "geburtsbriefe_1-99.csv")   
@@ -28,45 +26,20 @@ def split_and_write(d): # split text by delimiters and write to CSV table
     results=d.split("G /") # separates individual results from each other
     print(results[:10])
     
-    for result in results:
+    for result in results: # extracts different information from each result
         result_stripped=result.strip() # removes empty lines and whitespace at the end of each result
         if not result_stripped: 
             print("EMPTY")
-            continue # ignore results consisting only in one or two empty lines from data with list comprehension
+            continue # excludes results consisting only in whitespace or empty lines
         else:
-            result_string=str(result_stripped) # read result as string
-            result_clean=result_string.replace("\n", "").replace(",", "/").replace(";","/") # remove empty lines and replace special characters
-            chunks=re.split(regexPattern, result_clean) # extract different information types from result
-            print("\n TO BE ADDED TO CSV:", chunks)
-            with open(out_file, 'a+', newline='', encoding="utf-8") as csvoutput: # adds data without overwriting existing content
+            result_string=str(result_stripped) # reads valid result as string
+            result_clean=result_string.replace("\n", "").replace(",", "/").replace(";","/") # removes empty lines and replace special characters that mess up CSV
+            chunks=re.split(regexPattern, result_clean) # extracts different information types from result by Regex defined above
+            print("\n TO BE ADDED TO CSV:", chunks) # checks if list output is correct
+            with open(out_file, 'a', newline='', encoding="utf-8") as csvoutput: # adds data without overwriting existing content
                 writer=csv.writer(csvoutput, dialect="excel")
-                writer.writerow(chunks) # reach each set of bibliographic information to a new row
+                writer.writerow(chunks) # writes each set of bibliographic information per result to a new row
             
 split_and_write(data)
 
 print("done")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
